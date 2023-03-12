1eb83a4e146a3058700d9cc5ee7253c3610ae6f9
Igor Dyachenko <dvajta@gmail.com>
2023-03-12 19:37:55 +0300

    Brought back a random fix

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -17,10 +17,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <div class="row">
         <div class="col-lg-5">
-            <?php $form = ActiveForm::begin([
-                    'id' => 'login-form',
-                    'method' => 'POST'
-            ]); ?>
+            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
